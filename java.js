f23443cf87ddd33d074ab738913b2a6c0a19ddd9
SamsAcolyte <182761871+SamsAcolyte@users.noreply.github.com>
2024-11-12 11:23:52 +0300

    Group human win and lose conditions using ||

--- java.js
+++ java.js
@@ -1,10 +1,15 @@
 // Create variable computerChoice
     let computerChoice;
+
 // Create variable humanChoice
     let humanChoice;
+
+
 // Create function getComputerChoice
-function getComputerChoice(){
+    function getComputerChoice(){
+
     computerChoice= Math.random(computerChoice);
+
     if (computerChoice <= 0.333) {
         computerChoice ='rock'
     }
@@ -14,7 +19,7 @@ function getComputerChoice(){
     else {
         computerChoice = 'scissors'
     }
-}
+    }
 
 // Create function getHumanChoice
     function getHumanChoice (){
@@ -23,45 +28,37 @@ function getComputerChoice(){
 
 // Create function determineWinner
     function determineWinner () {
+
         // alert computerChoice
         alert (`computer chose ${computerChoice} and you chose ${humanChoice} `)
+        
+        
         // Draw
 
-        if ((computerChoice === 'rock' && humanChoice === 'rock')
+        if (
+            (computerChoice === 'rock' && humanChoice === 'rock')
             || (computerChoice === 'paper' && humanChoice === 'paper')
             || (computerChoice === 'scissors' && humanChoice === 'scissors')
-        )
+            )
         {
             alert ('Draw!')
         }
         
 
+        // Human win
 
-        // Computer chose rock
-
-        else if (computerChoice === 'rock' && humanChoice === 'paper') {
-            alert ('You win! Computer chose rock')
-        }
-        else if (computerChoice === 'rock' && humanChoice === 'scissors'){
-            alert ('You lose! Computer chose rock')
-        }
-
-
-        // Computer chose paper
-
-        else if (computerChoice === 'paper' && humanChoice === 'scissors'){
-            alert ('You win! Computer chose paper')
-        }
-        else if (computerChoice === 'paper' && humanChoice === 'rock'){
-            alert ('You lose! Computer chose paper')
+        else if (
+            (computerChoice === 'rock' && humanChoice === 'paper')
+            || (computerChoice === 'paper' && humanChoice === 'scissors')
+            || (computerChoice === 'scissors' && humanChoice === 'rock')
+        )
+        {
+            alert ('You win!')
         }
 
-        // computer chose scissors
+        // Human lose
 
-        else if (computerChoice === 'scissors' && humanChoice === 'rock'){
-            alert ('You win! Computer chose scissors')
-        }
-        else if (computerChoice === 'scissors' && humanChoice === 'paper'){
-            alert ('You lose! Computer chose scissors')
+        else {
+            alert ('You lose!')
         }
     }
