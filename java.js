c456fe11025dd965e90fcdb25f513b5c08664970
SamsAcolyte <182761871+SamsAcolyte@users.noreply.github.com>
2024-11-12 10:23:05 +0300

    Create basic pseudo code Create variable: computerChoice Create function: getComputerChoice
    function getComputerChoice:
    assigns variable computerChoice to random 0-1
    based on random number assigns variable computerChoice to rock-paper-scissors

---
+++ java.js
@@ -0,0 +1,21 @@
+// Create variable computerChoice
+    let computerChoice;
+// Create variable humanChoice
+    
+// Create function getComputerChoice
+function getComputerChoice(){
+    computerChoice= Math.random(computerChoice);
+    if (computerChoice <= 0.333) {
+        computerChoice ='rock'
+    }
+    else if (computerChoice > 0.333 && computerChoice <= 0.666){
+        computerChoice = 'paper'
+    }
+    else {
+        computerChoice = 'scissors'
+    }
+}
+
+// Create function getHumanChoice
+    
+// Create function determineWinner
